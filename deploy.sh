11eeccbbd5ebf246ac08e229215a822ebb122052
Tharaniguru <125856377+Tharaniguru@users.noreply.github.com>
2025-03-20 13:32:44 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u tharani0110 -p dckr_pat_6ZePnZQHuYlZw08jDSu6m7EWV80
-    docker tag test tharani0110/dev
-    docker push tharani0110/dev
+    docker tag test1 tharani0110/devopsgit
+    docker push tharani0110/devopsgit
     
     
